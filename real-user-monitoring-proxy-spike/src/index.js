f524b139ad42308fd73926a042a9ecf1cf178307
Christopher Bartling <chris.bartling@gmail.com>
2021-02-24 14:04:26 -0600

    Datadog RUM up and running.

--- real-user-monitoring-proxy-spike/src/index.js
+++ real-user-monitoring-proxy-spike/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {datadogRum} from '@datadog/browser-rum';
+
+datadogRum.init({
+  applicationId: process.env.REACT_APP_DATADOG_APPLICATION_ID,
+  clientToken: process.env.REACT_APP_DATADOG_CLIENT_TOKEN,
+  site: 'datadoghq.com',
+  service: 'DataDog RUM spike solution',
+  env: 'production',
+  version: '1.0.0',
+  sampleRate: 100,
+  trackInteractions: true
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
